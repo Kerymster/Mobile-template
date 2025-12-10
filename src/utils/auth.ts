6d4c1599660bb52541522f098f3fadd58fb8e69f
Kerim YILMAZ <kerim.yilmaz@directful.com>
2025-12-10 16:16:23 +0300

    fix: add reviewers note

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,5 +1,14 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+
+/**
+ * !!!! Important note for reviewers !!!!!
+ * 
+ * Due to some API issues, the profile token could not be used. However, all preparations for its use have been made. I request that this be taken into consideration during the evaluation process.
+ * 
+ * @returns The authentication token
+ */
+
 export const getAuthToken = async (): Promise<string | null> => {
   let token = await AsyncStorage.getItem('profileToken');
   if (!token) {
